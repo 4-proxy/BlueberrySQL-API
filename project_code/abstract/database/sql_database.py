1642830d4f6754a1d83ae596956e49e05fa79f76
4-proxy <pubsec.4proxy.k91@outlook.com>
2024-11-09 21:17:41 +0900

    A small fix for UML review and `SQLDataBase` completion.
    Major changes:
    - uml-review 0.2.2 -> 0.2.3
    - Edit `sql_database.py` 0.1.0 -> 0.2.0 (./project_code/abstract/database/);
    - Edit `test_sql_database.py` 0.1.0 -> 0.2.0 (./project_code/tests/test_abstract/);

--- project_code/abstract/database/sql_database.py
+++ project_code/abstract/database/sql_database.py
@@ -12,21 +12,39 @@ __all__: list[str] = [
 ]
 
 __author__ = "4-proxy"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from abc import ABC, abstractmethod
+
+from typing import Dict, Any
 
 
 # ______________________________________________________________________________________________________________________
 class SQLDataBase(ABC):
+    def __init__(self, **dbconfig) -> None:
+        self.dbconfig = dbconfig
+
+    # ------------------------------------------------------------------------------------------------------------------
+    @property
+    def dbconfig(self) -> Dict[str, Any]:
+        return self.__dbconfig
+
+    # ------------------------------------------------------------------------------------------------------------------
+    @dbconfig.setter
+    def dbconfig(self, new_dbconfig: Dict[str, Any]) -> None:
+        self.__dbconfig: Dict[str, Any] = new_dbconfig
+
+    # ------------------------------------------------------------------------------------------------------------------
     @abstractmethod
     def __repr__(self) -> str:
         pass
 
+    # ------------------------------------------------------------------------------------------------------------------
     @abstractmethod
-    def _get_info_about_server(self) -> None:
+    def _get_info_about_server(self) -> str:
         pass
 
+    # ------------------------------------------------------------------------------------------------------------------
     @abstractmethod
-    def _get_info_about_connection(self) -> None:
+    def _get_info_about_connection(self) -> str:
         pass
--- project_code/tests/test_abstract/test_sql_database.py
+++ project_code/tests/test_abstract/test_sql_database.py
@@ -8,13 +8,25 @@ Apache license, version 2.0 (Apache-2.0 license)
 """
 
 __author__ = "4-proxy"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 import unittest
 
 from abstract.database.sql_database import SQLDataBase as tested_class
 
-from typing import Any, Callable, List
+from typing import Any, Callable, Dict, List
+
+
+# ______________________________________________________________________________________________________________________
+class ConcreteTestClass(tested_class):
+    def __repr__(self) -> str:
+        return "__repr__"
+
+    def _get_info_about_connection(self) -> str:
+        return "_get_info_about_connection"
+
+    def _get_info_about_server(self) -> str:
+        return "_get_info_about_server"
 
 
 # ______________________________________________________________________________________________________________________
@@ -69,3 +81,23 @@ class TestSQLDataBase(unittest.TestCase):
 
                 # Check
                 self.assertTrue(expr=method.__isabstractmethod__)  # type: ignore
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test_constructor_initializes_private_field_dbconfig(self) -> None:
+        # Build
+        _class = ConcreteTestClass
+
+        expected_dbconfig: Dict[str, str] = {
+            "host": "localhost",
+            "user": "root",
+            "password": "password",
+        }
+
+        # Operate
+        instance = _class(**expected_dbconfig)
+
+        # Check
+        actual_dbconfig: Dict[str, Any] = instance.dbconfig
+
+        self.assertDictEqual(d1=expected_dbconfig,
+                             d2=actual_dbconfig)
